739e06bbbbd9387b9c254644dacac9427a022dfc
Kevin <17864518+Kmagameguy@users.noreply.github.com>
2021-12-19 18:24:27 -0500

    Use Github API to Find Latest Release

--- build.sh
+++ build.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
+
+set -Eeuo pipefail
+trap on_error ERR
+
+on_error() {
+  echo "Error on line $(caller): ${BASH_COMMAND}"
+}
+
+get_latest_snapraid_release() {
+  curl --silent "https://api.github.com/repos/amadvance/snapraid/releases/latest" | \
+    grep '"tag_name":' | \
+    sed -E 's/.*v([^"]+)".*/\1/'
+}
+
 APP_NAME="snapraid"
 IMAGE_TAG="$APP_NAME-build"
+LATEST_RELEASE_TAG="$(get_latest_snapraid_release)"
+BUILD_ARGS="--build-arg SNAPRAID_VERSION=${1:-$LATEST_RELEASE_TAG}"
 
 # Uncomment BUILD_PATH if using this Dockerfile as part of an Ansible deployment
 #BUILD_PATH="/tmp/build"
 #mkdir $BUILD_PATH
 #cd $BUILD_PATH
 
-BUILD_ARGS=""
-[[ -z ${1} ]] || BUILD_ARGS="--build-arg SNAPRAID_VERSION=$1"
-echo BUILD_ARGS=$BUILD_ARGS
+echo "BUILD_ARGS=$BUILD_ARGS"
 
 docker build -t $IMAGE_TAG $BUILD_ARGS .
 ID=$(docker create $IMAGE_TAG)
