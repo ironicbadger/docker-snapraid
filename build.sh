3cca946dd73ee93889cba8359d8fe01c2e65e0d4
Christian Artin <gravufo@gmail.com>
2022-03-24 11:49:50 -0400

    Update build.sh
    Build using -o to fetch the file instantly

--- build.sh
+++ build.sh
@@ -27,8 +27,4 @@ BUILD_ARGS="--build-arg SNAPRAID_VERSION=${1:-$LATEST_RELEASE_TAG}"
 
 echo "BUILD_ARGS=$BUILD_ARGS"
 
-docker build -t $IMAGE_TAG $BUILD_ARGS .
-ID=$(docker create $IMAGE_TAG)
-docker cp $ID:/build/ .
-docker rm -v $ID
-docker rmi $IMAGE_TAG
+docker build -o type=local,dest=./build/ $BUILD_ARGS .
